4b9cc90f89787a99dbac79a0c6d10a426df7dd4e
dbaim <79461183+dbaim@users.noreply.github.com>
2021-07-14 18:44:10 +0600

    Update bot.py
    Добавил выбор вкусов, количество товара

--- bot.py
+++ bot.py
@@ -1,55 +1,50 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-from config import TOKEN  
 
+from config import TOKEN
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import message
 from aiogram.utils import executor
 
 import markups as nav
 
-korzina:int = 0 # quantity zakaza 
+korzina: int = 0
+
 
 class Pozicia:
-    type:int
-    quantity:int
-    vkus:int
+    type: int
+    quantity: int
+    vkus: int
 
     def __init__(self, type, quantity, vkus):
         self.type = type
         self.quantity = quantity
-        self.vkus = vkus  
-    
+        self.vkus = vkus
+
     def ReturnPozicia(self):
         self.pozicia = self.vkus + " " + self.type + " " + self.quantity
-        return self.pozicia 
+        return self.pozicia
 
-    def ReturnQuantity(self):
-        return self.quantity
+
+pozicia: Pozicia
+
 
 class Zakaz:
     zakaz: list
-    quantity: int
 
-    def __init__(self, zakaz, quantity):
+    def __init__(self, zakaz) -> None:
         self.zakaz = zakaz
-        self.quantity = quantity
-    
+
 
 class Client:
-    Name:str
-    phone:str
-    address:str
-    Tov:Zakaz
-    def __init__(self,Name,phone, address, Tov):
-        self.Name = Name
-        self.phone = phone
-        self.address = address
-        self.Tov = Tov
-        
-# Фотографии из корневой папки 
+    Name: str
+    phone: str
+    address: str
+    Tov: Zakaz
+
+
+# Фотографии из корневой папки
 Elf800 = open("ElfBar800.jpg", 'rb')
 Elf1500 = open("ElfBar1500.jpg", 'rb')
 
@@ -57,74 +52,103 @@ Elf1500 = open("ElfBar1500.jpg", 'rb')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+
 # Команда /start
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
-    await bot.send_message(message.from_user.id, "Привет, {0.first_name}!\nЭтот телеграм бот для заказа электронных сигарет Elf Bar. Ниже вы найдете каталог товаров которые есть в наличии. Спасибо что выбираете нас.\nПриятных покупок!".format(
+    await bot.send_message(message.from_user.id,
+                           "Привет, {0.first_name}!\nЭтот телеграм бот для заказа электронных сигарет Elf Bar. Ниже вы найдете каталог товаров которые есть в наличии. Спасибо что выбираете нас.\nПриятных покупок!".format(
                                message.from_user), reply_markup=nav.mainMenu)
+
 
 # Навигация
 @dp.message_handler(text="⬅️ Главное меню")
 async def cmd_random(message: types.Message):
     await bot.send_message(message.from_user.id, 'Главное меню', reply_markup=nav.mainMenu)
 
- 
+
 @dp.message_handler(text="Каталог")
 async def cmd_random(message: types.Message):
     await bot.send_message(message.from_user.id, 'Выберите нужный товар', reply_markup=nav.KatMenu)
 
-    
+
 @dp.message_handler(text="Корзина")
 async def cmd_random(message: types.Message):
-     await bot.send_message(message.from_user.id, 'Корзина', reply_markup=nav.KorzMenu)
+    await bot.send_message(message.from_user.id, 'Корзина', reply_markup=nav.KorzMenu)
+
 
 # Каталог
-# Отправка фотографии и описания при нажатии 
-@dp.message_handler(text="ElfBar (800 Затяжек), Lux ElfBar (800 затяжек)")
+# Отправка фотографии и описания при нажатии
+@dp.message_handler(text="ElfBar (800 Затяжек) \n Lux ElfBar (800 затяжек)")
 async def cmd_random(message: types.Message):
-    await bot.send_photo(message.from_user.id, Elf800, caption="Одноразовая электронная сигарета Elfbar 800 от одноименной компании компании. Поставляется в индивидуальной упаковке с концентрацией 5% никотина. Приятная тугая затяжка и насыщенные вкусы позволят в полной мере насладится данным устройством. Работает при помощи авто-затяжки и позволяет совершать до 800 затяжек.")
-    #Должны быть кнопки со вкусами 
+    await bot.send_photo(message.from_user.id, Elf800,
+                         caption="Одноразовая электронная сигарета Elfbar 800 от одноименной компании компании. Поставляется в индивидуальной упаковке с концентрацией 5% никотина. Приятная тугая затяжка и насыщенные вкусы позволят в полной мере насладится данным устройством. Работает при помощи авто-затяжки и позволяет совершать до 800 затяжек.",
+                         reply_markup=nav.MainVkusMenu)
     global pozicia
-    pozicia = Pozicia(800, 0, "Strawberry ice")
+    pozicia=Pozicia(800, 0, "default")
 
-    
-@dp.message_handler(text="ElfBar (1500 Затяжек), Lux ElfBar (1500 затяжек)")
+@dp.message_handler(text="ElfBar (1500 Затяжек) \n Lux ElfBar (1500 затяжек)")
 async def cmd_random(message: types.Message):
-    await bot.send_photo(message.from_user.id, Elf1500, caption="Elf Bar 1500 – удобная и вкусная одноразка на 1500 затяжек от компании Elf. Одноразовая Pod система не требует абсолютно никакой подзарядки и дозаправки жидкости, все что требуется это распаковать под-систему с защитного блистера и насладиться сочным и насыщенным вкусом. Картридж вмещает в себя 4.8 мл жидкости с содержанием 50 мг никотина, что примерно равно 1500 затяжкам. Электронки должно хватить на 4-8 дней парения, все зависит от частоты использования.")
+    await bot.send_photo(message.from_user.id, Elf1500,
+                         caption="Elf Bar 1500 – удобная и вкусная одноразка на 1500 затяжек от компании Elf. Одноразовая Pod система не требует абсолютно никакой подзарядки и дозаправки жидкости, все что требуется это распаковать под-систему с защитного блистера и насладиться сочным и насыщенным вкусом. Картридж вмещает в себя 4.8 мл жидкости с содержанием 50 мг никотина, что примерно равно 1500 затяжкам. Электронки должно хватить на 4-8 дней парения, все зависит от частоты использования.",
+                         reply_markup=nav.MainVkusMenu)
+    global pozicia
+    pozicia = Pozicia(1500, 0, "default")
 
-    
-#Примерный расчет товара
-global quantity
-quantity = 0
-@dp.message_handler(quantity)#lambda message: not message.text.isdigit(), quantity 
-                             # разобраться
+
+# Вкусы
+@dp.message_handler(text="Перейти к вкусам")
 async def cmd_random(message: types.Message):
-    await bot.send_message(message.from_user.id, 'Введите нужное количество товара')
-    global korzina
-    korzina += 1
-    pozicia.quantity = quantity
-            
-#Корзина
+    await message.reply("Все вкусы представлены ниже", reply_markup=nav.VkusMenu)
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button1')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, 'Выбран вкус Strawberry ice', reply_markup=nav.MainKolMenu)
+    global pozicia
+    pozicia = Pozicia(0, 0, "Strawberry ice")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button2')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, 'Выбран вкус Mint', reply_markup=nav.MainKolMenu)
+    global pozicia
+    pozicia = Pozicia(0, 0, "Mint")
+
+
+@dp.callback_query_handler(lambda c: c.data == 'button3')
+async def process_callback_button1(callback_query: types.CallbackQuery):
+    await bot.send_message(callback_query.from_user.id, 'Выбран вкус Mango', reply_markup=nav.MainKolMenu)
+    global pozicia
+    pozicia = Pozicia(0, 0, "Mango")
+
+
+# Выбор количества
+@dp.message_handler(text="Перейти к выбору количества")
+async def cmd_random(message: types.Message, quantity=None):
+    await message.reply("Введите какое количество данной электронной сигареты вы хотите (макс. 15): ", quantity)
+    global pozicia
+    pozicia = Pozicia(0, quantity, "Strawberry ice")
+
+
+# Корзина
 @dp.message_handler(text="Оформить заказ")
 async def cmd_random(message: types.Message):
     await bot.send_message(message.from_user.id, 'Введите Ваш номер телефона (с +7)')
-    
-    
-    @dp.message_handler(lambda message:message.text.startswith("+7"), Client.phone)
+
+    @dp.message_handler(lambda message: message.text.startswith("+7"), Client.phone)
     async def cmd_random(message: types.Message):
         await bot.send_message(message.from_user.id, 'Введите ваш адрес')
-        
-        
+
         @dp.message_handler(Client.address)
         async def cmd_random(message: types.Message):
             await bot.send_message(message.from_user.id, 'Введите ваше Имя')
-            
-            
-            @dp.message_handler(Client.name)
+
+            @dp.message_handler(Client.Name)
             async def cmd_random(message: types.Message):
                 await bot.send_message(message.from_user.id, 'Готово!')
                 # На данном этапе класс клиента должен быть готов
-                # оплата заказа
+
 
 # Логика удаления и очищения корзины
 # @dp.message_handler(text="Удалить товар")
