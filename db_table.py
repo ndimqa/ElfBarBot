ce24b25e74d53b1929a13ff016f43bb76faeac3f
dbaim <79461183+dbaim@users.noreply.github.com>
2021-07-19 01:10:12 +0600

    Файл для парсинга из экселя

--- db_table.py
+++ db_table.py
@@ -1,14 +1,19 @@
-import sqlite3
+# import sqlalchemy
+# import pandas as pd
+# 
+# engine = sqlalchemy.create_engine('sqlite://Users/baimu/PycharmProjects/elfbarBot/Shop.db')  # Путь к бд
+# df = pd.read_excel('Tovary(800).xlsx')
+# df.rename(columns={
+#     'ID вкуса': 'vkus',
+#     'Количество': 'quantity'
+# })
+# 
+# df.to_sql (
+#     name='tovary(800)',
+#     con=engine,
+#     index=False,
+#     if_exists='append'
+# )
 
-with sqlite3.connect('Shop.db') as db:
-    cursor = db.cursor()
-    type_query1 = """ INSERT INTO type (id, type) VALUES (1, 800) """
-    type_query2 = """ INSERT INTO type (id, type) VALUES (2, 1500) """
-    vkusy_query1 = """ INSERT INTO vkusy (id, vkus) VALUES (1, "Strawberry ice") """
-    vkusy_query2 = """ INSERT INTO vkusy (id, vkus) VALUES (2, "Mint") """
-    vkusy_query3 = """ INSERT INTO vkusy (id, vkus) VALUES (2, "Mango") """
-    cursor.execute(type_query1)
-    cursor.execute(type_query2)
-    cursor.execute(vkusy_query1)
-    cursor.execute(vkusy_query2)
-    cursor.execute(vkusy_query3)
+
+# Скрипт для парсинга данных из эксель таблиц в SQL DB. Нужно переделать SQL таблицы, и сделать два файла на 800 и 1500 типов. Оттуда можно доставать переменную для проверки наличия товара, менять значения
